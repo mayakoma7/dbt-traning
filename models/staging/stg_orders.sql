5fbcab615dc3913775b49e8469516e582441cde7
mayakoma@gmail.com <mayakoma@gmail.com>
2025-07-22 15:30:34 +0000

    added delivery team seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -23,6 +23,6 @@ select
 from {{ ref('raw_orders') }} as o
     left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
     left join {{ ref('raw_product') }} as p on p.productid = o.productid
- --   {{limit_data_in_dev()}}
+
 
    
