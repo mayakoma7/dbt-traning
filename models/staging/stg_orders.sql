7142b57ba1eb9f19d85244b57a494c77bed40847
mayakoma@gmail.com <mayakoma@gmail.com>
2025-07-22 10:35:15 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,20 @@
+select 
+-- from raw orders
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice - o.ordercost as orderprofit,
+    o.ordercost,
+    o.ordersellingprice,
+-- from raw customer
+    c.customername,
+    c.segment,
+    c.country,
+-- from raw product
+    p.CATEGORY,
+    p.productname,
+    p.subcategory
+from {{ ref('raw_orders') }} as o
+    left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
+    left join {{ ref('raw_product') }} as p on p.productid = o.productid
